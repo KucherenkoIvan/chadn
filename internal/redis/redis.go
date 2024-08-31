f202bdd399cfbc9c0c35896818d2425168791290
Ivan Kucherenko <i.kucherenko@1er.app>
2024-08-31 23:45:59 +0500

    feat: setup basic redis client

--- internal/redis/redis.go
+++ internal/redis/redis.go
@@ -1,40 +1,19 @@
 package redis
 
 import (
-	"context"
-	"fmt"
-
 	"github.com/redis/go-redis/v9"
 )
 
-var ctx = context.Background()
+var client *redis.Client
 
-func ExampleClient() {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
-
-	err := rdb.Set(ctx, "key", "value", 0).Err()
-	if err != nil {
-		panic(err)
+func Client() *redis.Client {
+	if client == nil {
+		client = redis.NewClient(&redis.Options{
+			Addr:     "localhost:6379",
+			Password: "", // no password set
+			DB:       0,  // use default DB
+		})
 	}
 
-	val, err := rdb.Get(ctx, "key").Result()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("key", val)
-
-	val2, err := rdb.Get(ctx, "key2").Result()
-	if err == redis.Nil {
-		fmt.Println("key2 does not exist")
-	} else if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("key2", val2)
-	}
-	// Output: key value
-	// key2 does not exist
+	return client
 }
